27f720af8c88c5d917d796f5fd488fb4c8e2b849
Migs Arabejo <amarabejo0308@gmail.com>
2026-02-04 21:07:10 +0800

    Initial Spring Boot project setup

---
+++ src/test/java/com/miguel/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
